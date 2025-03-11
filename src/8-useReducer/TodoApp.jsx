db383076d6b059c88cdbeb49ec9529a1d3158e4d
graciamm16 <graciamartin007@gmail.com>
2025-03-11 09:38:16 +0100

    Proyecto actualizado

--- src/8-useReducer/TodoApp.jsx
+++ src/8-useReducer/TodoApp.jsx
@@ -1,46 +1,33 @@
-import { useReducer } from "react";
-import { todoReducer } from "./todoReducer";
 import {TodoList} from './TodoList';
 import {TodoAdd} from './TodoAdd';
-
-const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del tiempo',
-        done: false
-    }
-];
+import { useTodo } from '../hooks';
 
 export const TodoApp = () => {
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
-
-    const handleNewTodo = (todo) => {
-        dispatch({
-            type: 'add',
-            payload: todo
-        });
-    }
+    //useTodo()
+    // todos, handleDeleteTodo, handleToggleTodo, handleNewTodo
+    const {todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo} = useTodo();
 
     return (
         <>
-        <h1>TodoApp: 10, <small>Pendientes: 2</small></h1>
+        <h1>TodoApp: {todosCount}, <small>Pendientes: {pendingTodosCount}</small></h1>
         <hr/>
 
         <div className="row">
             <div className="col-7">
                 {/* TodoList */}
-                <TodoList todos = {todos}/>
+                <TodoList
+                    todos = {todos} 
+                    onDeleteTodo = {handleDeleteTodo}
+                    onToggleTodo={handleToggleTodo}
+                />
             </div>
             <div className="col-5">
                 <h4>Agregar TODO</h4>
                 <hr/>
                 {/* TodoAdd onNewTodo(todo)*/}
-                <TodoAdd onNewTodo = {handleNewTodo}/>
+                <TodoAdd
+                    onNewTodo = {handleNewTodo}
+                />
             </div>
         </div>
         </>
